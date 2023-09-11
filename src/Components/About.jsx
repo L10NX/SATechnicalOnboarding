90d3bedc7d3d7119611284f3f32591d6097b58ba
L10NX <125269818+L10NX@users.noreply.github.com>
2023-09-11 16:34:48 +0000

    x

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an IOC Analyst at NTT Ltd. 👨🏻‍💻";
+  "Azure Cloud Architect ☁️";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Microsoft Azure",
+  "Microsoft Teams",
+  "Cybersecurity",
+  "Cryptography",
+  "Forensic Analysis",
+  "OSINT",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I continue to expand my knowledge in the technology sector day by day, learning new skills and developing old ones.👣.";
 
 const About = () => {
   return (
