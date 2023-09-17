1002b3722fbf253400149504268e6622d11f1fc3
L10NX <125269818+L10NX@users.noreply.github.com>
2023-09-17 18:50:56 +0000

    change image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "/workspaces/SATechnicalOnboarding/src/images/google-cloud-datacenter-2-990x660-990x480.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Datacenter";
 
 const Home = ({ name, title }) => {
   return (
